ab3888ed112825c60d64d8de92fafcfd56ecca6f
eric-vinn <eric.vincent@ti.ukdw.ac.id>
2022-11-24 13:22:33 +0700

    commit4

--- soal2.py
+++ soal2.py
@@ -2,3 +2,8 @@ print ("========== PROGRAM PENGHITUNG PYTHAGORAS ==========")
 bbPertama = int(input("Masukkan bilngan bulat pertama: "))
 bbKedua = int(input("Masukkan bilangan bulat kedua: "))
 bbKetiga = int(input("Masukkan bilangan bulat ketiga: "))
+hasil = (bbPertama ** 2) + (bbKedua ** 2)
+if hasil == (bbKetiga**2):
+    print("Merupakan Pythagoras")
+else:
+    print("BUkan Merupakan Pythagoras")
