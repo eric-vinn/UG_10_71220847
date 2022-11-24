ff0a6730d8d8008e62145ced79182b482dd5fa44
eric-vinn <eric.vincent@ti.ukdw.ac.id>
2022-11-24 13:18:49 +0700

    commit3

---
+++ soal2.py
@@ -0,0 +1,4 @@
+print ("========== PROGRAM PENGHITUNG PYTHAGORAS ==========")
+bbPertama = int(input("Masukkan bilngan bulat pertama: "))
+bbKedua = int(input("Masukkan bilangan bulat kedua: "))
+bbKetiga = int(input("Masukkan bilangan bulat ketiga: "))
